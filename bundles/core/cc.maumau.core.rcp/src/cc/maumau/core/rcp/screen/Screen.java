d7aa8ff004da067460752224dcf19eb9f1d1d98d
Christian Kuka <christian@kuka.cc>
2016-05-03 22:03:29 +0200

    Add screen capture function

---
+++ bundles/core/cc.maumau.core.rcp/src/cc/maumau/core/rcp/screen/Screen.java
@@ -0,0 +1,22 @@
+package cc.maumau.core.rcp.screen;
+
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+
+/**
+*
+* @author Christian Kuka <christian@kuka.cc>
+*
+*/
+public class Screen {
+
+	public static BufferedImage capture(int delay) throws AWTException {
+		Robot robot = new Robot();
+		robot.delay(delay);
+		return robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+	}
+
+}
